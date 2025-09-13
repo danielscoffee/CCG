c294d2a962e116226ae2c19c1f8f1cf2eb69f115
Renato Fermi <repiazza@gmail.com>
2025-09-13 15:10:36 -0300

    fix: adjustment on indentation

--- include/shop.h
+++ include/shop.h
@@ -4,11 +4,11 @@
   #include <player.h>
   #include <monster.h>
 
-  #define SHOP_PRICE_UPGRADE   20
-  #define SHOP_PRICE_POISON    15
+  #define SHOP_PRICE_UPGRADE           20
+  #define SHOP_PRICE_POISON            15
 
-  #define SHOP_STD_GOLD_AMOUNT_REWARD 15
-  #define SHOP_GOLD_MULTIPLIYER       1.2
+  #define SHOP_STD_GOLD_AMOUNT_REWARD  15
+  #define SHOP_GOLD_MULTIPLIYER        1.2
 
   void vOpenShop(PSTRUCT_DECK pstDeck);
   void vAddPlayerReward(PSTRUCT_PLAYER pstPlayer);
